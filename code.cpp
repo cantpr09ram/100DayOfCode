d7d5240fe68955139ab35f034027c62d8dee8e16
bochainwu <bochainwu@gmail.com>
2022-07-06 14:32:16 +0800

    Q:a003 AC (2ms, 324KB)

--- code.cpp
+++ code.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main(){
-    int a, b;
-    cin >> a >> b;
-    cout << a+b;
+    int m, d, destiny;
+    cin >> m >> d;
+    destiny = (m*2+d)%3;
+
+    if (destiny == 0){
+        cout << "普通";
+    }else if (destiny == 1)
+    {
+        cout << "吉";
+    }else{
+        cout<<"大吉";
+    }
     return 0;
 }
