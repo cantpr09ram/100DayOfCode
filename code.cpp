fce6b1b735c3ed50dfc1b9b30b34eb4e7fbb7f4d
bochainwu <bochainwu@gmail.com>
2022-07-15 16:24:30 +0800

    not finished yet (infix to postfix)
    讀取資料應該成功了
    參考資料
    1 .https://www.youtube.com/watch?v=toDlC_JnrIg&ab_channel=%E8%95%AD-%E5%BF%97%E6%98%8E
    2 .https://github.com/JieHong-Liu/zerojudge/blob/master/.vscode/a017.cpp

--- code.cpp
+++ code.cpp
@@ -3,19 +3,64 @@
 #include<algorithm>
 #include<math.h>
 #include<vector>
+#include<stack>
+#include<queue>
+#include<sstream> 
 using namespace std;
 class Solution {
     public:
-        int a;
+        string a;
         void solution();
 };
 
+int weigh_calculator(char c, string now) {
+    char priority[7] = {'+', '-', '*', '/', '%', '(', ')'};
+    int weight_s[7] = {1, 1, 2, 2, 2, 0, -2};
+    int weight_c[7] = {1, 1, 2, 2, 2, 4, -2};
+
+    if (now == "isp"){
+        for (int i = 0; i < 7; i++){
+            if (c == priority[i]){
+                return weight_s[i];
+            }
+        }
+    }else if(now == "icp"){
+        for (int i = 0; i < 7; i++){
+            if (c == priority[i]){
+            return weight_c[i];
+            }
+        }
+    }
+    return -1;
+}    
+
 void Solution::solution(){
-    cout << a - 1; 
+    stack<char> operation;
+    stack<int> result;
+    queue<string> postfix_queue;
+    istringstream iss(a);
+    string token;  
+    while( iss>>token ){  
+        cout << token[0];
+        if(weigh_calculator(token[0], "isp") == -1){
+            postfix_queue.push(token);
+        }else{
+            while(weigh_calculator(token[0], "icp") <= weigh_calculator(operation.top(), "isp")){
+                string temp(1, operation.top());;
+                if (temp != ")" || temp != "("){
+                    postfix_queue.push(temp);
+                }
+                operation.pop();   
+            }
+            operation.push(token[0]);
+        }
+    }
+    
 }
 int main(void){
     Solution x;
-    cin >> x.a;
-    x.solution();
+    while(getline(cin, x.a)){
+        x.solution();
+    }
     return 0;
 }
