b953fc90aaff39b6b6de90d1e39dfbd397d16ba9
bochainwu <bochainwu@gmail.com>
2022-07-28 16:50:37 +0800

    Q:a021 	NA (score:56%)
    1. 負數
    2 . 除法

--- code.cpp
+++ code.cpp
@@ -12,38 +12,78 @@ class Solution {
     public:
         string a, b;
         char op;
-        void solution();
+        void solution();   
+        void scan(string s, int a[]);
+        void print(int a[]);
+        bool less_than(int a[], int b[]);
 };
 
+void Solution::scan(string s, int a[]){
+    int i = 500 - 1; // 大數的數字位置
+    int j = 0, n = s.length();// 字串的字元位置
+    while (i >= n) a[i--] = 0;// 開頭一律填零
+    while (i >= 0) a[i--] = s[j++] - '0';// 字串頭尾顛倒，存入陣列
+}
+void Solution::print(int a[500]){
+    int i = 500 - 1;
+    while (i >= 0 && a[i] == 0) i--;
+
+    if (i < 0)
+        cout << '0';
+    else
+        while (i >= 0) cout << a[i--];
+}
+
 void Solution::solution(){
-    //string to number
-    int arr_a[500] = {}, arr_b[500] = {}, ans[500] = {};
-    int len_a = a.length();
-    int gap = 500 - len_a;
-    while(true){
-        if (len_a == -1){
-            break;
-        }else{
-            arr_a[len_a + gap] = a[len_a];
-            len_a -- ;
-            cout << len_a <<" "<< arr_a[len_a + gap] <<" "<< a[len_a]<<endl;
+    //string to array
+    int arr_a[500], arr_b[500], ans[500] = {};
+    scan(b, arr_b);
+    scan(a, arr_a);
+
+    switch (op)
+    {
+    case '+':
+        for(int i = 0; i < 500; i++){
+            ans[i] += arr_a[i] + arr_b[i];
+            if(ans[i] > 10){
+                ans[i] %= 10;
+                ans[i+1] += ans[i] / 10;
+            }
         }
-    }
-    int len_b = b.length();
-    gap = 500 - len_b;
-    while(true){
-        if (len_b == -1){
-            break;
-        }else{
-            arr_b[len_b + gap] = b[len_b];
-            len_b -- ;
+        print(ans);
+        break;
+    case '-':
+        for(int i = 0; i < 500; i++){
+            ans[i] = arr_a[i] - arr_b[i];
+            if(ans[i] < 0){
+                ans[i] += 10;
+                ans[i+1] --;
+            }
         }
+        for(int i = 0; i < 500; i++){
+            if (ans[i] < 0){
+                
+            }
+        }
+    case '*':
+        for(int i = 0; i < 500; i++){
+            for(int j = 0; j < 500; j++){
+                ans[i+j] += arr_a[i]*arr_b[j];
+            }
+        }
+        for(int i = 0; i < 499; i++){
+            ans[1+i] += ans[i] / 10;
+            ans[i] = ans[i] % 10;
+        }
+        print(ans);
+        break;
+    case '/':
+
+        break;
+    default:
+        break;
     }
-    int i = 500;
-    while(arr_a[i] != 0){
-        cout << arr_a[i];
-        i--;
-    }
+
 }
 int main(void){
     Solution x;
