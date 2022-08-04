5cb8f96a288ecb62ceb5054548f361457f31b809
bochainwu <bochainwu@gmail.com>
2022-08-04 15:16:23 +0800

    Roman to integer runtime: 63 ms memory: 14MB

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-#Palindrome Number
-def solution(a:int) -> bool:
-    b = str(a)
-    for i in range(len(b)):
-        if b[i] != b[len(b) - i - 1]:
-            return False
-    return True
-a = 101
-x = solution(a)
-print(x)
+#Roman to Integer
+def romanToInt(s: str) -> int:
+        ans = 0
+        for i in range(len(s)):
+            if s[i] == 'I':
+                ans += 1
+            elif s[i] == 'V':
+                if s[i - 1] == 'I' and i > 0:
+                    ans -= 2
+                ans += 5
+            elif s[i] == 'X':
+                if s[i - 1] == 'I' and i > 0:
+                    ans -= 2
+                ans += 10
+            elif s[i] == 'L':
+                if s[i - 1] == 'X' and i > 0:
+                    ans -= 20
+                ans += 50
+            elif s[i] == 'C':
+                if s[i - 1] == 'X' and i > 0:
+                    ans -= 20
+                ans += 100
+            elif s[i] == 'D':
+                if s[i - 1] == 'C' and i > 0:
+                    ans -= 200
+                ans += 500
+            elif s[i] == 'M':
+                if s[i - 1] == 'C' and i > 0:
+                    ans -= 200
+                ans += 1000
+            else:
+                continue
+            print(s[i])
+            print(ans)
+        return ans
+s = "MMMCDXC"
+a = romanToInt(s)
+print(a)
