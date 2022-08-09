bb58d942e9c9b7bbecb73b147ef146bed92b331e
bochainwu <bochainwu@gmail.com>
2022-08-09 11:19:05 +0800

    Longest Common Prefix runtime: 32ms memory: 14MB

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 #Longest Common Prefix
-def longestCommonPrefix(self, strs: List[str]) -> str:
-    a = zip(*strs)
-    ans = ''
-    for i in  a:
-        if len(set(i)) == 1:
-            ans += i[0]
+def longestCommonPrefix(strs: list[str]) -> str:
+    ans = ""
+    if len(strs[0]) == 0:
+        return ans
+    else:
+        a = zip(*strs)
+        for i in  a:
+            print(len(set(i)))
+            if len(set(i)) != 1:
+                return str(ans)
+            elif len(set(i)) == 1:
+                ans += i[0]
     return ans
+a = longestCommonPrefix(["a"])
+print(a)
