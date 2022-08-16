0d2c42767ff5a90e7af201af54b19ad8528422a1
bochainwu <bochainwu@gmail.com>
2022-08-16 21:02:53 +0800

    searchInsert runtime: 52ms momory: 14.6MB

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack) - len(needle) + 1):
-            if haystack[i: i+ len(needle)] == needle:
-                return i
-        return -1
+def searchInsert(nums: list[int], target: int) -> int:
+    for i in range(len(nums)):
+        if nums[i]== target:
+            return i
+            print('a')
+        elif i < len(nums) - 1 and (nums[i] < target and nums[i+1] >= target):
+                return i + 1
+                print('b')
+    return len(nums)
+            
+
+a = [1,2,3,4,6]
+b = searchInsert(a, 5)
+print(b)
