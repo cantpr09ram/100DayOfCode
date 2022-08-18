9956645ab9f641393480c1499825ef98ce51a9bb
bochainwu <bochainwu@gmail.com>
2022-08-18 11:47:13 +0800

    addBinary runtime: 39ms memory: 14MB

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        a_len = len(digits)
-        ans = 0
-        for i in range(a_len):
-            ans += digits[i] * (10**(a_len-i - 1))
-        return [i for i in str(ans+1)]
+    def addBinary(self, a: str, b: str) -> str:
+        len_a = len(a)
+        len_b = len(b)
+        len_max = max(len_a,len_b)
+        a = a.zfill(len_max)
+        b = b.zfill(len_max)
+        result = ''
+        carry = 0
+        for i in range(len_max - 1, -1, -1):
+            r = carry
+            r += 1 if a[i] == '1' else 0
+            r += 1 if b[i] == '1' else 0
+            
+            result = ('1' if r % 2 == 1 else '0') + result
+            carry = 0 if r < 2 else 1
+
+        if carry != 0:
+            result = '1' + result
+
+        return result
