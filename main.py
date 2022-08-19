fbcb4c842a2bc7e79aa5cbe085e1fdc5d5be76c5
bochainwu <bochainwu@gmail.com>
2022-08-19 09:44:26 +0800

    climbStairs runtime: 63ms memory: 13.8MB

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 class Solution:
-    def mySqrt(self, x: int) -> int:
-        return int(x ** 0.5)
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        current = 2
+        pre = 1
+    # 當前臺階的走法是前兩個臺階走法之和
+        for i in range(2,n):
+            current = current + pre
+            pre = current - pre
+        return current
