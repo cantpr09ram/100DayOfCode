41d721d2df10090f4622ec07f6a82e674dbfed7b
bochainwu <bochainwu@gmail.com>
2022-09-15 21:22:57 +0800

    a012. 10055  AC (5ms, 336KB)

--- code.cpp
+++ code.cpp
@@ -1,51 +1,12 @@
-/*
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
 int main(void){
-    string str;
-    
-    while(getline(cin, str)){
-        int ans = 0;
-        for (auto s : str){
-            if(s == ' '){
-                ans += 1;
-            }
-        }
-        cout << ans+1 <<endl;
+    long long int a, b;
+    while(cin >> a >> b){
+        cout << abs(a - b);
     }
+    
     return 0;
 }
-*/
-#include <iostream>
-#include <string>
-using namespace std;
-int main(void)
-{
-    //ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int sum;
-    string a;
-    bool con,b;
-    
-    while(getline(cin,a))
-    {
-        sum=0;
-        con=false;
-        for(auto i:a)
-        {
-            b=isalpha(i);
-            if(b && !con)
-            { 
-                sum++;
-                con=true;
-            } 
-            else if(!b)
-                con=false; 
-        }
-        cout<<sum<<"\n";
-    }
-    return 0;
-}
-
- 
