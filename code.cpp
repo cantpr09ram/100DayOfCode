ba17813b7f3db2ccbf7837de9fae40d7f243b264
bochainwu <bochainwu@gmail.com>
2022-09-16 23:51:58 +0800

    d073 	AC (2ms, 340KB)

--- code.cpp
+++ code.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
 using namespace std;
-
-int main(int argc, char** argv) {
-	int n;
-	while(cin >> n){
-		int ans = 0;
-		for(int i = 0; i<n; i++){
-			ans += (n - i)*(n - i);
-		}
-		if (ans != 0){
-			cout << ans << endl;
-		}
-		
-	}
-	return 0;
+int main() {
+  int n;
+  cin  >> n;
+  cout << ((n+2) / 3);
+  return 0;
 }
