fee363557def46f8aedc38259a6a00ad652d2b03
bochainwu <bochainwu@gmail.com>
2022-09-20 23:27:34 +0800

    fix: ; in the end of line 6

--- code.cpp
+++ code.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main() {
   string boy, gril;
   cin >> gril>>boy;
-  cout <<gril <<" and "<<boy<<" sitting in the tree"
+  cout <<gril <<" and "<<boy<<" sitting in the tree";
   return 0;
 }
