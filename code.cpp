6917651aeb456b0363b4675e672c18672157d443
bochainwu <bochainwu@gmail.com>
2022-09-23 09:01:33 +0800

    c328 	AC (2ms, 332KB)

--- code.cpp
+++ code.cpp
@@ -3,17 +3,24 @@
 using namespace std;
 
 int main() {
-  int h1, m1, h2, m2, start, end, ans;
-  cin >> h1 >> m1;
-  cin >> h2 >> m2;
-  start = h1*60 + m1;
-  end = h2*60 + m2;
-  if (start > end) {
-    end += 60*24;
-    //cout <<"hi"<<endl;
+  int a, b;
+  char op;
+  cin >> a >> op >> b;
+  switch(op){
+    case '+':
+      cout<< a + b;
+      break;
+    case '-':
+      cout << a - b;
+      break;
+    case '*':
+      cout<< a * b;
+      break;
+    case '/':
+      cout<< a / b;
+      break;
+    default:
+      cout <<"no";
   }
-  //cout <<end <<" "<< start<<endl;
-  ans = end - start;
-  cout << ans / 60 <<" "<< ans % 60;
   return 0;
 }
