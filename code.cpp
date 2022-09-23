23c13a7aa821b514bcdb7415f542af1257fb5de1
bochainwu <bochainwu@gmail.com>
2022-09-23 09:50:50 +0800

    d071 	AC (3ms, 316KB)

--- code.cpp
+++ code.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main() {
   int year;
-  cin >> year;
-  if (((year % 4 ==0)&&(year % 100 != 0))||(year %400 == 0)){
-    cout<<"a leap year";
-  }else{
-    cout<<"a normal year";
+  while(cin >> year){
+    if (((year % 4 ==0)&&(year % 100 != 0))||(year %400 == 0)){
+      cout<<"a leap year"<<endl;
+    }else{
+      cout<<"a normal year"<<endl;
+    }
   }
+  
   return 0;
 }
