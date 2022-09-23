2843b6429822e50c585b99034b782e4db2edf48d
bochainwu <bochainwu@gmail.com>
2022-09-23 08:55:30 +0800

    b682 AC (3ms, 336KB)

--- code.cpp
+++ code.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 
 int main() {
-  int h, m;
-  cin >> h >> m;
-  if ((h*60 + m > 450) && (h*60 + m < 1020)) cout<<"At Schoo"<<endl;
-  else cout <<"Off School"<<endl;
+  int h1, m1, h2, m2, start, end, ans;
+  cin >> h1 >> m1;
+  cin >> h2 >> m2;
+  start = h1*60 + m1;
+  end = h2*60 + m2;
+  if (start > end) {
+    end += 60*24;
+    //cout <<"hi"<<endl;
+  }
+  //cout <<end <<" "<< start<<endl;
+  ans = end - start;
+  cout << ans / 60 <<" "<< ans % 60;
   return 0;
 }
