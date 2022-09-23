cc189651daccf78af824f5766e0b2b03f5788751
bochainwu <bochainwu@gmail.com>
2022-09-23 09:58:27 +0800

    d072 AC (4ms, 312KB)

--- code.cpp
+++ code.cpp
@@ -5,6 +5,7 @@ int main() {
   int year, num;
   cin >>num;
   for(int i =0; i<num;i++){
+    cout <<"Case "<<i+1<<": ";
     cin >> year;
     if (((year % 4 ==0)&&(year % 100 != 0))||(year %400 == 0)){
       cout<<"a leap year"<<endl;
