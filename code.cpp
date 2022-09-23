7bf1cf4e9e1e5b6a0e3c1964d2387a0143328d85
bochainwu <bochainwu@gmail.com>
2022-09-23 09:13:14 +0800

    f423 AC (2ms, 320KB)

--- code.cpp
+++ code.cpp
@@ -3,24 +3,14 @@
 using namespace std;
 
 int main() {
-  int a, b;
-  char op;
-  cin >> a >> op >> b;
-  switch(op){
-    case '+':
-      cout<< a + b;
-      break;
-    case '-':
-      cout << a - b;
-      break;
-    case '*':
-      cout<< a * b;
-      break;
-    case '/':
-      cout<< a / b;
-      break;
-    default:
-      cout <<"no";
+  int b, ans = 0;
+  cin >>b;
+  for(int i = 0; i < b+1; i++){
+    if (i % 2 != 0){
+      ans += i;
+      //cout<<ans<<endl;
+    }
   }
+  cout << ans;
   return 0;
 }
