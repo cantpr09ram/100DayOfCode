d0fd1e48d96046cce552e7fe4bdfd28d97652661
bochainwu <bochainwu@gmail.com>
2022-09-23 09:33:45 +0800

    b294 	AC (4ms, 324KB)

--- code.cpp
+++ code.cpp
@@ -3,21 +3,14 @@
 using namespace std;
 
 int main() {
-  int a,b,d;
-  cin >> a >> b >> d;
-  if(a < b){
-  while(a <= b){
-      cout << a <<" ";
-      a = a + d;
-    }
-    cout << endl;
-  }else if(a == b){
-      cout << a << endl;
-  }else{
-      while(b <= a){
-      cout << a <<" ";
-      a = a + d;
-      }
+  int day, num1 = 0, num2, ans = 0;
+  cin >> day;
+  while(num1 < day){
+    cin >> num2;
+    num1 += 1;
+    ans += num2 * num1;
   }
+  cout << ans;
+
   return 0;
 }
