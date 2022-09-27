b81859fb9dd1b9212443de2263441a5dc3c4a914
bochainwu <bochainwu@gmail.com>
2022-09-27 22:49:39 +0800

    b882 	NA (score:60%) format output number(without 0 in front)

--- code.cpp
+++ code.cpp
@@ -3,19 +3,13 @@
 using namespace std;
 
 int main(void){
-  int c;
-  cin >> c;
-  for(int i = 0; i< c; i++){
-    int num;
-    cin >> num;
-    int shop[num] = {};
-    for(int j = 0; j < num; j++){
-      cin >> shop[j];
-    }
-    //cout <<shop[2];
-    int max = *max_element(shop, shop+sizeof(shop) / sizeof(int)), min = *min_element(shop, shop+sizeof(shop) / sizeof(int));
-    //cout << max << min;
-    cout << (max - min)*2<<endl;
-  }
+  long long int h, m, s;
+  cin >>h >> m >> s;
+  m += s / 60;
+  s = s % 60;
+  h += m / 60;
+  m = m % 60;
+  h = h % 24;
+  printf("%s:%s:%s",h,m,s);
   return 0;
 }
