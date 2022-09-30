5499b4b54b8c7e9856973acd32ad4f4458a8fa7c
bochainwu <bochainwu@gmail.com>
2022-09-30 09:30:56 +0800

    a024 	AC (2ms, 324KB)

--- code.cpp
+++ code.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
-#include <algorithm>
- #include<iomanip>
+#include<iostream>
 using namespace std;
 
-int main(void){
-  long long int h, m, s;
-  cin >>h >> m >> s;
-  m += s / 60;
-  s = s % 60;
-  h += m / 60;
-  m = m % 60;
-  h = h % 24;
-  cout<<setfill('0')<<setw(2)<<h<<":"<<setfill('0')<<setw(2)<<m<<":"<<setfill('0')<<setw(2)<<s;
-  //printf("%lld:%lld:%lld",h,m,s);
-  return 0;
+int main(){
+    int a, b, t;
+    while( cin >> a >> b ){
+        while( b!=0 ){
+            t = b;
+            b = a%b;
+            a = t;
+            //swap a and b
+        }
+        cout << a << endl;
+    }
+    return 0;
 }
