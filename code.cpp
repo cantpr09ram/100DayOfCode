2f264e8629f1d0b52bd89c0c3f884e21b9a62d5b
bochainwu <bochainwu@gmail.com>
2022-10-05 00:00:38 +0800

    c420 	AC (4ms, 324KB)

--- code.cpp
+++ code.cpp
@@ -4,15 +4,19 @@ using namespace std;
 int main(){
   int num, ans;
   while(cin >> num){
+    int star = 1, dash = num - 1 - 1;
     for(int i = 0; i < num; i++){
-      for(int j = 0; j < num; j++){
-        if(j < num -i -1){
-          cout<<"_";
-        }else{
-          cout<<"*";
+      for(int j = 0; j < num*2 - 1;j++){
+        if(j <= dash + star && j > dash){
+          cout << '*';
+        }
+        else{
+          cout << '_';
         }
       }
       cout<<endl;
+      star += 2;
+      dash -= 1;
     }
   return 0;
   }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-  int num, ans;
-  while(cin >> num){
-    for(int i = 0; i < num; i++){
-      for(int j = 0; j < i+1; j++){
-        cout << '*';
-      }
-      cout<<endl;
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+int N;
+scanf("%d",&N);
+        int k=N-1,p=N-(N-1);
+    for(int i=0;i<N;i++)
+    {
+        for(int l=0;l<k;l++)
+        {
+            printf("_");
+        }    
+    for(int u=0;u<p;u++)
+    {
+        printf("*");
+    
+        
     }
-  }
-  return 0;
+    for(int y=0;y<k;y++)
+    {
+        printf("_");
+        }
+        printf("\n");
+        p=p+2;
+            k=k-1;
+
+    }
 }
+
+ 
