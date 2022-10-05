0e02aba8c89a20947e95d1dda9862071c3e82f26
bochainwu <bochainwu@gmail.com>
2022-10-06 01:31:33 +0800

    b968 	AC (2ms, 340KB)

--- code.cpp
+++ code.cpp
@@ -2,15 +2,8 @@
 using namespace std;
 
 int main(){
-  int a;
-  cin>>a;
-  for(int i = 0; i<a;i++){
-    int a, f, ans = 0;
-    cin>>a;
-    cin>>f;
-    for(int j = a; j<f+1;j++){
-      if (j%2 != 0) ans +=j;
-    }
-    cout<<"Case "<<i+1<<": "<<ans<<endl;
-  }
+  string a, b;
+  getline(cin,a);
+  getline(cin,b);
+  cout<<a<<" and "<<b<<" sitting in the tree";
 }
