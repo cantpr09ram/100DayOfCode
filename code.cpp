90ed4f5e19a6779676c0ac853d0a5707d29e641c
bochainwu <bochainwu@gmail.com>
2022-10-07 08:50:10 +0800

    a053 	AC (3ms, 340KB)

--- code.cpp
+++ code.cpp
@@ -1,21 +1,19 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
+using namespace std;
 int main(){
-  int num;
-  cin >> num;
-  for(int i = 0; i < num; i++){
-    int start, stop, gap, park = 0;
-    cin >> start >> stop >> gap;
-    for(int i = start+1; i < stop; i++){
-      if (i % gap != 0) {
-        cout << i << " ";
-        park = 1;
-      }
-    }
-    if(park == 0){
-      cout<<"No free parking spaces.";
-    }
-    cout<<endl;
+  int n;
+  cin >> n;
+  if(n <= 10){
+    cout << n * 6 << endl;
+  }else if((n > 10) && (n <= 20)){
+    cout << (n - 10) * 2 + 60 << endl;
+  }else if((n > 20) && (n <= 40)){
+      cout << (n - 20) + 80 << endl;
+  }else{
+    cout << "100" <<endl;
   }
+  return 0;
 }
+
+ 
