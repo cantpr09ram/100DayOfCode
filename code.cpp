29db1fbec87b58723edb508f2464c42268176a15
bochainwu <bochainwu@gmail.com>
2022-10-07 08:55:42 +0800

    d460 	AC (3ms, 340KB)

--- code.cpp
+++ code.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
-
 using namespace std;
 int main(){
   int n;
   cin >> n;
-  if(n <= 10){
-    cout << n * 6 << endl;
-  }else if((n > 10) && (n <= 20)){
-    cout << (n - 10) * 2 + 60 << endl;
-  }else if((n > 20) && (n <= 40)){
-      cout << (n - 20) + 80 << endl;
+  if(n <= 5){
+    cout << 0 << endl;
+  }else if((n > 5) && (n <= 11)){
+    cout << 590 << endl;
+  }else if((n > 11) && (n <= 17)){
+      cout << 790 << endl;
+  }else if(n >= 60){
+    cout << 399 <<endl;
   }else{
-    cout << "100" <<endl;
+    cout<<890<<endl;
   }
   return 0;
 }
