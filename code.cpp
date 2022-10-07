341b50cf185cda6a93a6ce9022e8b008f192baa7
bochainwu <bochainwu@gmail.com>
2022-10-07 18:03:52 +0800

    e622 	NA (score:98%)

--- code.cpp
+++ code.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 using namespace std;
 int main(){
-  int n;
-  cin >> n;
-  if(n <= 5){
-    cout << 0 << endl;
-  }else if((n > 5) && (n <= 11)){
-    cout << 590 << endl;
-  }else if((n > 11) && (n <= 17)){
-      cout << 790 << endl;
-  }else if(n >= 60){
-    cout << 399 <<endl;
-  }else{
-    cout<<890<<endl;
+  int n, dust, max, maxcp = 0;
+  cin >> n>>dust;
+  for(int i = 0; i < n; i++){
+    int cp, iv, use = dust;
+    cin >> cp >> iv;
+    while(use > 1000){
+      use -= 1000;
+      if( iv<= 29){
+        cp += 10;
+      }else if ((iv >= 30) && (iv <39)){
+        cp += 50;
+      }
+      else{
+        cp += 100; 
+      }
+    }
+
+    if(cp > maxcp){
+      maxcp = cp;
+      max = i;
+    }
   }
+  cout<<max+1<<" "<<maxcp;
   return 0;
 }
 
