63e5f3bae6566fd8ab7a49045f513ddd8dd9d998
bochainwu <bochainwu@gmail.com>
2022-10-12 21:14:56 +0800

    f044 	AC (3ms, 340KB)

--- code.cpp
+++ code.cpp
@@ -2,18 +2,13 @@
 #include <algorithm>
 using namespace std;
 int main(){
-  int a, b, eq;
-  cin >> eq >> a;
-  if (a!= eq){
-    b = eq - a;
-  }else{
-    a = a-3;
-    b = 3;
+  int king, small, count, day;
+  cin>> king >> small;
+  while(count < small){
+    count += count;
+    count += king;
+    day++;
   }
-  if(a > b){
-    cout<<b<<"+"<<a<<"="<<eq;
-  }else{
-    cout<<a<<"+"<<b<<"="<<eq;
-  }
+  cout<<day;
   return 0;    
 }
