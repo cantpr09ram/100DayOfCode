dec589186d29ac9e92bebdb5e3d8a850b29c944b
bochainwu <bochainwu@gmail.com>
2022-10-12 21:03:52 +0800

    f043 	AC (3ms, 348KB)

--- code.cpp
+++ code.cpp
@@ -2,37 +2,18 @@
 #include <algorithm>
 using namespace std;
 int main(){
-  int num, max_ans = 0, ans = 0,sum = 0, temp;
-  string player;
-  cin >> num >> player;
-  int len_player = player.length();
-  int player_num [len_player] = { 0 };
-
-  for(int i = 0; i<len_player; i++){
-    //player_num[i] = player_num[i] * 10 + (player[i] - 48);
-    player_num[len_player - i -1] = player[i] - 48;
+  int a, b, eq;
+  cin >> eq >> a;
+  if (a!= eq){
+    b = eq - a;
+  }else{
+    a = a-3;
+    b = 3;
   }
-  
-  for(int i = 0; i<len_player; i++){
-      sum += player_num[i];
-      //cout <<player_num[i]<<endl;
-      if(((i+1) % num == 0) || (i == len_player - 1)){
-        if(max_ans <= sum){
-          if((i == len_player - 1) && ((i + 1) % num != 0)){
-            temp = (i+1)/num + 1;
-            //cout<<temp<<endl;
-          }else{
-            temp = (i+1) / num;
-          }
-          ans = max(temp, ans);
-          max_ans = sum;
-          //cout << ans <<" "<< max_ans<<endl;
-        }
-        sum = 0;
-     }
+  if(a > b){
+    cout<<b<<"+"<<a<<"="<<eq;
+  }else{
+    cout<<a<<"+"<<b<<"="<<eq;
   }
-  
-  cout << ans <<" "<< max_ans;
   return 0;    
 }
-//5 10121315
