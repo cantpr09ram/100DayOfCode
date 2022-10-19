2fea0e26a6a2e47aaed4f963199a4ba7dba6912f
bochainwu <bochainwu@gmail.com>
2022-10-20 00:36:49 +0800

    f707 AC (4ms, 344KB)

--- code.cpp
+++ code.cpp
@@ -3,14 +3,40 @@ using namespace std;
 #define int long long
 //f707
 main (){
-    cin.tie(0); cout.sync_with_stdio(false);
-    int N;
-    stack<int> numbers;
-    while (cin>>N) {
-      cout<<"hi"<<numbers.size()<<N<<endl;
-      if (!N) {
-        cout<<"end"<<numbers.size();
-        break;
-      }else numbers.push(N);
+  cin.tie(0); cout.sync_with_stdio(false);
+  int N;
+  stack<int> numbers;
+  while (cin>>N){
+    if (!N) {
+      break;
+    }else numbers.push(N);
+  }
+
+  queue<int> div, undiv;
+  while (numbers.size()) {
+    numbers.top()%7==0 ? div.push(numbers.top()):undiv.push(numbers.top());
+    numbers.pop();
+  }
+  int max = 0, temp;
+  if(div.size()){
+    temp = 0;
+    while (div.size()){
+      if(div.front()%70 >= temp){
+        temp = div.front()%70;
+        max = div.front();
+      }
+      //cout<<div.front()<<endl; //this 
+      div.pop();
     }
+  }else{
+    temp = 78;
+    while(undiv.size()){
+      if(undiv.front() % 77 < temp){
+        temp = undiv.front() % 77;
+        max = undiv.front();
+      }
+    undiv.pop();
+    }
+  }
+  cout<<max;
 }
