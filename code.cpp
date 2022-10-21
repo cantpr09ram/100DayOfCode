1d253955eb749835d5edc2bb16730e69c39353ac
bochainwu <bochainwu@gmail.com>
2022-10-21 09:22:32 +0800

    e968 	AC (10ms, 340KB)

--- code.cpp
+++ code.cpp
@@ -2,27 +2,13 @@
 #include <iomanip>
 using namespace std;
 int main(void){
-  int num;
+  int num, a, b, c;
   cin>>num;
-  for(int i=0; i<num;i++){
-    int a;
-    int index[4] = {0};
-    for(int i=0;i<4;i++){
-      cin>>index[i];
-      char c = getchar();
-      if(c == '\n') break;
+  cin>>a>>b>>c;
+  for(int i=num; i>0;i--){
+    if(((i != a) && (i != b)) && (i != c)){
+      cout<<"No. "<<i<<endl;
     }
-    double x;
-    if(index[0] == 0){
-      x = (9.6*index[3]) + (1.8*index[2]) - (4.7*index[1]) + 655;
-      
-    }else{
-      x = (13.7*index[3]) + (5.0*index[2]) - (6.8*index[1]) + 66;
-    }
-    cout<<setiosflags(ios::fixed);
-    cout<<setprecision(2) <<x<<endl;
-    //cout.unsetf( ios::fixed );
-    
   }
   return 0;
 }
