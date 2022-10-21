b0323f108e9c9af8bad3cf4e57e785df756ee38c
bochainwu <bochainwu@gmail.com>
2022-10-21 08:59:17 +0800

    e948 AC (7ms, 352KB)

--- code.cpp
+++ code.cpp
@@ -1,24 +1,28 @@
 #include <bits/stdc++.h>
+#include <iomanip>
 using namespace std;
 int main(void){
-  int a, b, asum = 0,bsum = 0;
-  int x[10000], y[10000];
-  cin >>a>>b;
-  for(int i=0;i<a;i++){
-    cin>>x[i];
-    asum += x[i];
-    char c = getchar();
-    if(c == '\n') break;
+  int num;
+  cin>>num;
+  for(int i=0; i<num;i++){
+    int a;
+    int index[4] = {0};
+    for(int i=0;i<4;i++){
+      cin>>index[i];
+      char c = getchar();
+      if(c == '\n') break;
+    }
+    double x;
+    if(index[0] == 0){
+      x = (9.6*index[3]) + (1.8*index[2]) - (4.7*index[1]) + 655;
+      
+    }else{
+      x = (13.7*index[3]) + (5.0*index[2]) - (6.8*index[1]) + 66;
+    }
+    cout<<setiosflags(ios::fixed);
+    cout<<setprecision(2) <<x<<endl;
+    //cout.unsetf( ios::fixed );
+    
   }
-  for(int i=0;i<b;i++){
-    cin>>y[i];
-    bsum += y[i]; 
-    char c = getchar();
-    if(c == '\n') break;
-  }
-  
-  if((a > b) && (asum > bsum)){
-    cout<<"Yes";
-  }else cout<<"No";
   return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
-#include <algorithm>
-using  namespace std;
+#include <iomanip> 
+using namespace std;
+int main()
+{
+	cout << "test 1 =======" << endl;
+	double f = 3.1415926535;
+	cout << f << endl; // 3.14159
+	cout << setiosflags(ios::fixed); //只有在這項設置後，setprecision纔是設置小數的位數。
+	cout << setprecision(0) << f << endl; //輸出0位小數，3
+	cout << setprecision(1) << f << endl; //輸出1位小數，3.1
+	cout << setprecision(2) << f << endl; //輸出2位小數，3.14
+	cout << setprecision(3) << f << endl; //輸出3位小數，3.142
+	cout << setprecision(4) << f << endl; //輸出4位小數，3.1416
 
-int main(){
-    stack<int> num;
-    int a;
-    while(cin>>a){
-        num.push(a);
-    }
+	cout << "test 2 =======" << endl;
+	//cout.setf跟setiosflags一樣，cout.precision跟setprecision一樣
+	float a = 0.546732333;
+	float b = 3.563768245;
+	cout << a << endl;
+	cout << b << endl;
+	cout.setf(ios::fixed);
+	cout.precision(3);
+	cout << a << endl;
+	cout << b << endl;
+	cout.precision(1);
+	cout << a << endl;
+	cout << b << endl;
+	return 0;
 }
