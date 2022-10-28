276980265b9d3aee69855a4a0f2b3aa635786c6d
bochainwu <bochainwu@gmail.com>
2022-10-28 09:26:22 +0800

    d186 	AC (3ms, 312KB)

--- code.cpp
+++ code.cpp
@@ -1,23 +1,19 @@
 #include<iostream>
 using namespace std;
-  int main(){
-  int a, now = 1, b = 0;
 
-  while(cin>>a){
-    while(now < a){  
-    if(a%now == 0){
-      b += now;
-      //cout<<now<<endl;
-    }
-      now++;
-      //cout<<now<<endl;
-      //cout<<b<<endl;
-    }
-    if(a > b) cout<<"虧數"<<endl;
-    else if(a == b) cout<<"完全數"<<endl;
-    else cout<<"盈數"<<endl;
-    now = 1;
-    b = 0;
+int main(){
+int from = 0, end = 0;
+while(1){
+  int j,count = 0;
+  cin >> from >> end;
+  if(from == 0 && end ==0){
+    break;
+  }
+  for(j = 1; j * j < from; j++);
+    for(; j * j <= end ; j++){
+      count++;
+  }
+  cout << count << endl;
   }
   return 0;
 }
