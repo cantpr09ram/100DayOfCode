58f5ff67cb8347ac698c1f2094a4649eea1203b3
bochainwu <bochainwu@gmail.com>
2022-10-28 09:35:37 +0800

    a536 	AC (2ms, 324KB)

--- code.cpp
+++ code.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 
 int main(){
-int from = 0, end = 0;
-while(1){
-  int j,count = 0;
-  cin >> from >> end;
-  if(from == 0 && end ==0){
-    break;
-  }
-  for(j = 1; j * j < from; j++);
-    for(; j * j <= end ; j++){
-      count++;
-  }
-  cout << count << endl;
+  int a;
+  cin>>a;
+  for(int i=0; i<a;i++){
+    int a, b, c, total, f = 0;
+    cin>>a>>b>>c;
+    total = a+b;
+    while(total>=c){
+      int drink = total / c;
+      f += drink;
+      total %=c;
+      total += drink;
+    }
+    cout<<f<<endl;
   }
   return 0;
 }
