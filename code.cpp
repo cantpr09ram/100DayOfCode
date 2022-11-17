b969d11ce0ed850533b77a957a6db8317c0027a9
bochainwu <bochainwu@gmail.com>
2022-11-17 23:27:06 +0800

    g275  	AC (7ms, 344KB)

--- code.cpp
+++ code.cpp
@@ -1,47 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{   
-  int a,b,n;
-  int ans = 0 ;
-  cin>>a>>b;
-  cin>>n;
-    
-  for(int i=0;i<n;i++)
-  {
-    int input ;
-    int a_in = 0 ;
-    int b_in = 0 ; 
-    
-    while (cin>>input)
-    {
-      if (input==a)
-      {
-        a_in++ ;
-      }
-      else if (input== -a)
-      {
-        a_in-- ;
-      }
-      else if (input== b)
-      {
-        b_in++ ;
-      }
-      else if (input== -b)
-      {
-        b_in-- ;
-      }
-      char ch = getchar();
-      if(ch == '\n')
-      {
-        break;
-      }
+
+int main(){
+  int n, a[8], b[8];
+  cin >> n;
+
+  while (n--){
+    bool blown=true;
+
+    for (int i=1; i<=7; i++){
+      cin >> a[i];
     }
-        
-    if((a_in>=1)&&(b_in>=1))
-    {
-      ans++;
+
+    for (int i=1; i<=7; i++){
+      cin >> b[i];
     }
-  }
-  cout << ans ;
+    
+    if (a[2]==a[4] || a[2]!=a[6] || b[2]==b[4] || b[2]!=b[6]){
+      cout <<"A";
+      blown=false;
+    }
+    if (!a[7] || b[7]){
+      cout <<"B";
+      blown=false;
+    }
+    if (!(a[2]^b[2])+!(a[4]^b[4])+!(a[6]^b[6])){
+      cout <<"C";
+      blown=false;
+    }
+    cout << (blown ? "None\n" : "\n");
+}
+
 }
