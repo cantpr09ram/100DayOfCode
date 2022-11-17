7444376b09453f8125c9be6237c15aaa4e28212d
bochainwu <bochainwu@gmail.com>
2022-11-17 22:40:33 +0800

    f579 	AC (4ms, 336KB)

--- code.cpp
+++ code.cpp
@@ -1,64 +1,47 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
-{
-  int F,N;
-  cin >>F>>N;
-  int c[N],d[N];
-
-  d[0]=F;
-  for(int i=0;i<N;i++){
-    cin >> c[i];
-  }
-  d[1]=c[0];
-  for(int i=2;i<N;i++){
-    d[i]=c[i-1];
-  }
-  for(int i=2;i<N;i++){
-    if(c[i-2]==c[i-1]&&c[i-1]==0){
-      d[i]=5;
-    }
-    else if(c[i-2]==c[i-1]&&c[i-1]==2){
-      d[i]=0;
-    }
-    else if(c[i-2]==c[i-1]&&c[i-1]==5){
-      d[i]=2;
-    }
-  }
-
-  for(int i=0;i<N;i++){
-    if(c[i]==0&&d[i]==5){
-      cout << d[i]<<" : Won at round "<< i+1 << endl;
-      break;
-    }
-    if(c[i]==2&&d[i]==0){
-      cout << d[i]<<" : Won at round "<< i+1 << endl;
-      break;
-    }
-    if(c[i]==5&&d[i]==2)
+{   
+  int a,b,n;
+  int ans = 0 ;
+  cin>>a>>b;
+  cin>>n;
+    
+  for(int i=0;i<n;i++)
+  {
+    int input ;
+    int a_in = 0 ;
+    int b_in = 0 ; 
+    
+    while (cin>>input)
     {
-      cout << d[i]<<" : Won at round "<< i+1 << endl;
-      break;
+      if (input==a)
+      {
+        a_in++ ;
+      }
+      else if (input== -a)
+      {
+        a_in-- ;
+      }
+      else if (input== b)
+      {
+        b_in++ ;
+      }
+      else if (input== -b)
+      {
+        b_in-- ;
+      }
+      char ch = getchar();
+      if(ch == '\n')
+      {
+        break;
+      }
     }
-    if(c[i]==5&&d[i]==0)
+        
+    if((a_in>=1)&&(b_in>=1))
     {
-      cout << d[i]<<" : Lost at round "<< i+1 << endl;
-      break;
+      ans++;
     }
-    if(c[i]==0&&d[i]==2)
-    {
-      cout << d[i]<<" : Lost at round "<< i+1 << endl;
-      break;
-    }
-    if(c[i]==2&&d[i]==5){
-      cout << d[i]<<" : Lost at round "<< i+1 << endl;
-      break;
-    }
-    cout << d[i] << " ";
   }
-  if(d[N-1]==c[N-1]){
-    cout <<": Drew at round "<< N << endl;
-  }
-
-  return 0;
+  cout << ans ;
 }
