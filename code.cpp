1b7f1ccd6b7ed47412f3f43c09e5d5452ebd9f6e
bochainwu <bochainwu@gmail.com>
2022-12-01 23:23:47 +0800

    a693 	AC (0.7s, 824KB)

--- code.cpp
+++ code.cpp
@@ -1,35 +1,31 @@
-#include <iostream>
-#include <algorithm>
+#include<iostream>
 using namespace std;
-int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  int a[20] = {0};
-  int b[20] = {0};
-  int m, n, ans = 0;
-  cin>>n>>m;
-  for(int i = 0;i<n;i++){
-    
-    for(int j = 0;j<m;j++){
-      cin>>a[j];
-    }
-    ans += *max_element(a , a + 20);
-    b[i] = *max_element(a , a + 20);
-  }
-  cout<<ans<<endl;
-  int x = 0;
-  for(int i =0; i<20;i++){
-    if((b[i] != 0)&&(ans % b[i] == 0)){
-      if(x == 0){
-        x = 1;
-        cout<<b[i];
-      }else if(x == 1){
-        cout<<" "<<b[i];
-      }
-    }
-  }
-  if(x == 0){
-    cout<<-1;
-  }
-  return 0;
+int main()
+{
+    int n,m,c1[100000],c2[100000],l,r,i,sum=0;
+    c1[0]=0;
+    c2[0]=0;    
+    while(cin>>n)                    
+    {
+        cin>>m;
+        for(i=1;i<=n;i++)
+        {
+            cin>>c1[i];
+            if(i>=2)
+            {
+                c2[1]=c1[1];  
+                c2[i]=c2[i-1]+c1[i];
+            }
+        }
+        for(i=1;i<=m;i++)    
+        {
+            while(cin>>l>>r)
+            {
+                cout<<c2[r]-c2[l-1]<<endl;
+            }
+        }
+    } 
+    return 0;
 }
+
+ 
