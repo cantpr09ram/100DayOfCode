b3da1893f65cb1af0448627eaeb4f9d0fbd991a0
bochainwu <bochainwu@gmail.com>
2022-12-02 09:19:01 +0800

    g595 	AC (2ms, 344KB)

--- code.cpp
+++ code.cpp
@@ -3,25 +3,28 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    int n, friends = 0, j, k, a[50000];
+    int ans = 0, n;
+    int a[100];
     cin>>n;
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    for(int i = 0;i<n;i++){
-        if(a[i] != -1){
-            j = a[i];
-            while(true){
-                k = j;
-                j = a[j];
-                a[k] = -1;
-                if(j == i){
-                    break;
+    for(int i=0;i<n;i++){
+        if(a[i] == 0){
+            if(i == 0){
+                ans += a[1];
+            }else if(i == n-1){
+                ans += a[n-2];
+            }else{
+                if(a[i-1] > a[i+1]){
+                    ans += a[i+1];
+                }else{
+                    ans += a[i-1];
                 }
             }
-            friends++;
         }
     }
-    cout<<friends;
+    cout<<ans;
     return 0;
 }
+//g595
