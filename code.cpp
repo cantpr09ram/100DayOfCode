38ae07812f0ebc1fad7bd1539a7993a5d684764f
bochainwu <bochainwu@gmail.com>
2022-12-02 10:20:15 +0800

    b964 not finish yet

--- code.cpp
+++ code.cpp
@@ -4,18 +4,28 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    int a, b, c;
-    int num[9] = {0};
-    cin>>a>>b>>c;
-    num[a-1] += 1;
-    num[b-1] += 1;
-    num[c-1] += 1;
-    cout<<*max_element(num, num+9);
-    for(int i=8;i>=0;i--){
-        if(num[i] > 0){
-            cout<<" "<<i+1;
+    int a, best = 0, worst = 0;
+    int num[20] = {0};
+    cin>>a;
+    for(int i =0;i<a;i++){
+        cin>>num[i];
+    }
+    sort(num,num+20,greater<int>());
+    for(int i =0;i<a;i++){
+        if(a-i-1 == 20-a){
+            cout<<num[a-i-1];
+        }else{
+            cout<<num[a-i-1]<<" ";
         }
     }
+    cout<<endl;
+    for(int i =0;i<a;i++){
+        if((num[a-i-1] > 60)&&(i>0)){
+            cout<<num[a-i]<<endl;
+            best = 1;
+        }
+    }
+    if(best)
     return 0;
 }
 //i399
