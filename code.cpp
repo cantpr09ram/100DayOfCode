f6e79ca144229440eb37990e14c9305fd8a74b1a
bochainwu <bochainwu@gmail.com>
2022-12-06 16:15:04 +0800

    d563 	AC (14ms, 4.1MB)

--- code.cpp
+++ code.cpp
@@ -4,22 +4,22 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    int n, sum = 0, ans = 0;
-    int a1[1000000] = {0}, a2[1000000] = {0};
+    int n, sum = 0, ans = 0, prefix, suffix;
+    int a1[1000000] = {0};
     cin>>n;
     for(int i=0;i<n;i++){
         cin>>a1[i];
-        sum += a1[i];
-        a2[i] = sum;
     }
-    sum  = 0;
-    for(int i = n-1;i>=0;i--){
-        sum += a1[i];
-        int *f = find(a2, a2+1000000, sum);
-        if(f == a2+1000000){
-            continue;
-        }else{
+    prefix = a1[0];
+    suffix = a1[n-1];
+    for (int i = 0,j = n-1;i < n && j >= 0;) {
+        if (prefix == suffix) {
             ans += 1;
+            prefix += a1[++i];
+        } else if (prefix > suffix) {
+            suffix += a1[--j];
+        } else if (suffix > prefix) {
+            prefix += a1[++i];
         }
     }
     cout<<ans;
