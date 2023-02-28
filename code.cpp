88a188156823706a47b11c00c29f202e30ea570b
bochain <bochainwu@gmail.com>
2023-02-28 10:53:35 +0800

    f580 	AC (4ms, 364KB)

--- code.cpp
+++ code.cpp
@@ -1,49 +1,37 @@
 #include<iostream>
+#include<vector>
+
 using namespace std;
 int main(){
-    int s,t,n,m,r;
-    cin>>s>>t>>n>>m>>r;
-    int A[s][t];
-    int B[n][m];
-    int A_sum=0;
-    for(int i=0;i<s;i++){
-        for(int j=0;j<t;j++){
-            cin>>A[i][j];
-            A_sum+=A[i][j];
-        }   
+    cout.sync_with_stdio(false);
+    int n = 0, m = 0;
+    int dice[20][3];
+    cin>>n>>m;
+
+    for(int i = 0;i<n;i++){
+        for(int j=0; j<3;j++){
+            dice[i][j] = j+1;
+        }
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cin>>B[i][j];
-        }   
+
+    for(int i = 0;i<m; i++){
+       int a, b;
+       cin>>a>>b;
+
+        if (a*b >= 0){
+            swap(dice[a-1], dice[b-1]);
+        }else if (b == -1){
+            int top = dice[a-1][0];
+            dice[a-1][0] = dice[a-1][2];
+            dice[a-1][2] = 7 - top;
+        }else if (b == -2){
+            int right = dice[a-1][1];
+            dice[a-1][1] = dice[a-1][0];
+            dice[a-1][0] = 7 - right;
+        }
     }
-     
-    int total=0;
-    int minn=2000000;
-    for(int i=0;i<n-s+1;i++){
-        for(int j=0;j<m-t+1;j++){
-            int not_same=0;
-            int small_sum=0;
-            for(int y=0;y<s;y++){
-                for(int z=0;z<t;z++){
-                    if(B[y+i][z+j]!=A[y][z]){
-                        not_same++;
-                    }
-                    small_sum+=B[y+i][z+j];
-                }
-            }
-             
-            if(not_same<=r){
-                total++;
-                int diff=abs(small_sum-A_sum);
-                if(diff<minn){
-                    minn=diff;
-                }
-            }
-        }   
+    for(int i = 0;i<n;i++){
+        cout<< dice[i][0]<<" ";
     }
-    cout<<total<<endl;
-    if(total==0)cout<<-1<<endl;
-    else cout<<minn<<endl;
     return 0;
 } 
