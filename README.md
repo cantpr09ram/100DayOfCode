68bea949d643dce2a9e8246be746760ecf08cb76
bochain <bochainwu@gmail.com>
2023-02-28 16:23:45 +0800

    f606 	AC (3ms, 368KB)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # 100DayOfCode
 
 [problem](https://docs.google.com/spreadsheets/d/1m3PsJACKsJfFxSsRz2xE5zsaqTc6iBwp6l2Ml0tokBc/edit#gid=1468689738)
+
+	
+* f606
+* g276
--- code.cpp
+++ code.cpp
@@ -1,37 +1,39 @@
 #include<iostream>
 #include<vector>
-
+#include<algorithm> 
 using namespace std;
 int main(){
     cout.sync_with_stdio(false);
-    int n = 0, m = 0;
-    int dice[20][3];
-    cin>>n>>m;
-
-    for(int i = 0;i<n;i++){
-        for(int j=0; j<3;j++){
-            dice[i][j] = j+1;
+    int n,m,k; cin>>n>>m>>k;
+    vector<int> flow[n],ANS;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            int x; cin>>x;
+            flow[i].push_back(x);
         }
     }
-
-    for(int i = 0;i<m; i++){
-       int a, b;
-       cin>>a>>b;
-
-        if (a*b >= 0){
-            swap(dice[a-1], dice[b-1]);
-        }else if (b == -1){
-            int top = dice[a-1][0];
-            dice[a-1][0] = dice[a-1][2];
-            dice[a-1][2] = 7 - top;
-        }else if (b == -2){
-            int right = dice[a-1][1];
-            dice[a-1][1] = dice[a-1][0];
-            dice[a-1][0] = 7 - right;
+    for(int i=0;i<k;i++){
+        vector<int> router[m],plan(n);
+        for(int &p:plan) cin>>p;
+        for(int j=0;j<n;j++) 
+            router[plan[j]].push_back(j);
+        int ans=0;
+        for(int from=0;from<m;from++){
+            for(int to=0;to<m;to++){
+                int someflow = 0;
+                for(int j: router[from]) {
+                    someflow += flow[j][to];
+                }
+                if(from == to) {
+                    ans += someflow; 
+                }else {
+                    if(someflow<=1000) ans+=someflow*3;
+                    else ans+=3000+(someflow-1000)*2;
+                }
+            }
         }
+        ANS.push_back(ans);
     }
-    for(int i = 0;i<n;i++){
-        cout<< dice[i][0]<<" ";
-    }
+    cout<<*min_element(ANS.begin(),ANS.end())<<"\n";
     return 0;
-} 
+}
