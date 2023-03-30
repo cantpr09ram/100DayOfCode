55e028585088a620bdef3de946a7e30e0c7e84db
bochain <bochainwu@gmail.com>
2023-03-30 09:18:04 +0800

    i400 not finish yet

--- code.cpp
+++ code.cpp
@@ -1,52 +1,55 @@
 #include<iostream>
 #include<vector>
+#include <deque>
 using namespace std;
 int main(){
-    int n,m,x;
+    int m, n;
+    string e;
+    vector <string> arr;
+    string str;
     cin>>n>>m;
-    vector<int>S(n+1,-1),T(n+1,-1),idx,times(n+1,0);
-    for(int i=1;i<=n;i++) cin>>S[i];
-    for(int i=1;i<=n;i++) cin>>T[i];
-    for(int i=0;i<n;i++) cin>>x, idx.push_back(x);
+    for(int i=0;i<n;i++) {
+        cin>>e;
+        arr.push_back(e);
+    }
+    cin>>str;
 
-    while (idx.size() > 1){
-        vector<int>winner,loser;
-        for(int i=0;i<(int)idx.size();i+=2){
-            int player1,player2;
-            if(i+1 == idx.size()){
-                winner.push_back(idx[i]);
-                continue;
+    for(int i=0;i<n;i++) {
+        int cnt=0;
+        deque<char>dq;
+        for(int j=0;j<m;j++){
+            if(arr[i][j]=='1'){
+                cnt++;
+                dq.push_back(str[j]);
             }
             else{
-                player1=idx[i];
-                player2=idx[i+1];
-            }
-            long long a=S[player1], b=T[player1];
-            long long c=S[player2], d=T[player2];
-            if(a*b >= c*d){
-                winner.push_back(player1);
-                S[player1] = a+(c*d)/(2*b);
-                T[player1] = b+(c*d)/(2*a);
-                times[player2]++;
-                if(times[player2] < m)
-                    loser.push_back(player2);
-                S[player2] = c+(c/2);
-                T[player2] = d+(d/2);
-            }else{
-                winner.push_back(player2);
-                S[player2] = c+(a*b)/(2*d);
-                T[player2] = d+(a*b)/(2*c);
-                times[player1]++;
-                if(times[player1] < m)
-                    loser.push_back(player1);
-                S[player1] = a+(a/2);
-                T[player1] = b+(b/2);
+                dq.push_front(str[j]);
             }
         }
-        idx.clear();
-        for(int i: winner) idx.push_back(i);
-        for(int i : loser) idx.push_back(i);
+        string rt;
+        if(cnt%2==0){
+            for(char st:dq)rt+=st;
+        }else{
+            if(str.size()%2==0){
+                int mid=str.size()/2;
+                for(int k=mid;k<str.size();k++)rt+=dq[k];
+                for(int k=0;k<mid;k++)rt+=dq[k];
+            }
+            else{
+                int mid=str.size()/2;
+                for(int k=mid+1;k<str.size();k++)rt+=dq[k];
+                rt+=dq[mid];
+                for(int k=0;k<mid;k++)rt+=dq[k];
+            }
+        }
+        str = rt;
     }
-    cout<<idx[0];
+    cout<<str;
     return 0;
 }
+//BACDA
+//BCAAD
+//CABAD
+//10110
+//dacba
+//bacda
