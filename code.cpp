fd6ffa823dfa91d3918de5a24e868949171bf045
bochain <bochainwu@gmail.com>
2023-03-31 01:16:06 +0800

    not finish yet

--- code.cpp
+++ code.cpp
@@ -1,55 +1,37 @@
 #include<iostream>
 #include<vector>
-#include <deque>
 using namespace std;
 int main(){
-    int m, n;
-    string e;
-    vector <string> arr;
-    string str;
-    cin>>n>>m;
-    for(int i=0;i<n;i++) {
-        cin>>e;
-        arr.push_back(e);
+    int k,q,r;
+    cin>>k>>q>>r;
+    string step;
+    char ans[25][26];
+    for(int i=0;i<k;i++){
+        cin>>ans[0][i];
     }
-    cin>>str;
+    
+    
+    for(int i=0;i<q;i++){
+        for(int j=0;j<k;j++) {
+            int x; cin >> x;
+            ans[i+1][j] = ans[i][x-1];
+            cout<<ans[i][x-1]<<" "<<ans[i+1][j]<<" "<<x;
+        }
+        cout<<endl;
+    }
 
-    for(int i=0;i<n;i++) {
-        int cnt=0;
-        deque<char>dq;
-        for(int j=0;j<m;j++){
-            if(arr[i][j]=='1'){
-                cnt++;
-                dq.push_back(str[j]);
-            }
-            else{
-                dq.push_front(str[j]);
-            }
+    for(int i=0;i<q;i++){
+        for(int j=0;j<k;j++) {
+            cout<<ans[i+1][j];
         }
-        string rt;
-        if(cnt%2==0){
-            for(char st:dq)rt+=st;
-        }else{
-            if(str.size()%2==0){
-                int mid=str.size()/2;
-                for(int k=mid;k<str.size();k++)rt+=dq[k];
-                for(int k=0;k<mid;k++)rt+=dq[k];
-            }
-            else{
-                int mid=str.size()/2;
-                for(int k=mid+1;k<str.size();k++)rt+=dq[k];
-                rt+=dq[mid];
-                for(int k=0;k<mid;k++)rt+=dq[k];
-            }
-        }
-        str = rt;
+        cout<<endl;
     }
-    cout<<str;
+    /*
+    for(int i=1;i<=r;i++){
+        for(int j=0;j<q;j++) {
+            cout<<ans[j][i];
+        }
+        cout<<endl;
+    }*/
     return 0;
 }
-//BACDA
-//BCAAD
-//CABAD
-//10110
-//dacba
-//bacda
