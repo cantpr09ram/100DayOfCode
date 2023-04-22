8b875102633da4b39d5eee143556aa59188398f8
bochain <bochainwu@gmail.com>
2023-04-22 10:12:25 +0800

    h660 	AC (3ms, 328KB)

--- code.cpp
+++ code.cpp
@@ -1,26 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int k, w, s, e, price = 0;
-    cin>>k>>w>>s>>e;
-    if(k > 2) price += 20 + (k-2)*5;
-    else price += 20;
+    int x, r, v, n, p, s;
+    cin>>x>>r>>v;
+    cin>>n;
 
-    price += w/2 * 5;
-
-    for(int i=s; i<=e; i++){
-        if(i==18 && e>=19){
-            price += 185;
-        }else if(i==19 && e>=20){
-            price += 195;
-        }else if(i==20 && e>=21){
-            price += 205;
-        }else if(i==21 && e>=22){
-            price += 215;
-        }else if(i==22 && e>=23){
-            price += 225;
+    for(int i=0; i<n;i++){
+        cin>>p>>s;
+        if(abs(x - p) <= r && s<=v){
+            x = p;
+        }else if(abs(x - p) <= r && s>v){
+            if(x > p){
+                x += 15;
+            }else{
+                x -= 15;
+            }
         }
+        //cout<<x<<endl;
     }
-    cout<<price;
+    cout<<x;
     return 0;
 }
