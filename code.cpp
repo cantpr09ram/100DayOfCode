1734fb4276a78a7fc92af79b06535d1e873f2db6
bochain <bochainwu@gmail.com>
2023-04-23 09:57:59 +0800

    f345 	AC (2.3s, 4.1MB)

--- code.cpp
+++ code.cpp
@@ -1,23 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x, r, v, n, p, s;
-    cin>>x>>r>>v;
-    cin>>n;
-
-    for(int i=0; i<n;i++){
-        cin>>p>>s;
-        if(abs(x - p) <= r && s<=v){
-            x = p;
-        }else if(abs(x - p) <= r && s>v){
-            if(x > p){
-                x += 15;
-            }else{
-                x -= 15;
-            }
-        }
-        //cout<<x<<endl;
+    int num;
+    cin>>num;
+    int arr[num];
+    for(int i=0;i<num;i++){
+        cin>>arr[i];
     }
-    cout<<x;
+
+    for(int i=num-1;i>=0;i--){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
