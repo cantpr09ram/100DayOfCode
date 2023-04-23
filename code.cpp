f4655443410b5c095dd9ce806cf0d35111b71043
bochain <bochainwu@gmail.com>
2023-04-24 07:56:31 +0800

    b558 	AC (3ms, 324KB)

--- code.cpp
+++ code.cpp
@@ -1,15 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int num;
-    cin>>num;
-    int arr[num];
-    for(int i=0;i<num;i++){
-        cin>>arr[i];
-    }
-
-    for(int i=num-1;i>=0;i--){
-        cout<<arr[i]<<" ";
+    int n;
+    while(cin>>n){
+        cout<<1+n*(n-1)/2<<endl;
     }
     return 0;
 }
