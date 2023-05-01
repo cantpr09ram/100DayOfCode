f24db9d1d9c4501626e290e0363884eca01f2fbc
bochain <bochainwu@gmail.com>
2023-05-01 08:45:39 +0800

    d107 AC (2ms, 324KB)

--- code.cpp
+++ code.cpp
@@ -2,22 +2,31 @@
 #include<cmath>
 using namespace std;
 int main(){
-    int n, ans=1000001, min_wight, min_hight;
-    cin>>n;
-    long long int wight[n], hight[n];
-    for(int i=0;i<n;i++){
-        cin>>hight[i];
+    string word;
+    int lucky, maxn=0, minn=101, letter[26] = {0};
+    cin>>word;
+    for(int i=0;i<word.length();i++){
+        letter[(int)word[i] - 97] ++; 
     }
-    for(int i=0;i<n;i++){
-        cin>>wight[i];
+
+    for(int i=0; i<26;i++){
+        if(letter[i] > maxn) maxn = letter[i];
+        if(letter[i] < minn && letter[i] > 0) minn = letter[i];
     }
-    for(int i=0;i<n;i++){
-        if(wight[i]*hight[i] <= ans){
-            ans = wight[i]*hight[i];
-            min_hight = hight[i];
-            min_wight = wight[i];
+    bool isPrime = false;
+    int prime[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+    for(auto p:prime){
+        if(maxn - minn == p){
+            isPrime = true;
         }
     }
-    cout<<min_hight<<" "<<min_wight;
+    if(isPrime){
+        cout<<"Lucky Word"<<endl;
+        cout<<maxn - minn;
+    }else{
+        cout<<"No Answer"<<endl;
+        cout<<'0';
+    }
+    
     return 0;
 }
