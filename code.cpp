0590e5695666793402d4e773b7aa722233da41f1
bochain <bochainwu@gmail.com>
2023-05-02 22:48:21 +0800

    e327 AC (2ms, 340KB)

--- code.cpp
+++ code.cpp
@@ -1,19 +1,12 @@
 #include<iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
 using namespace std;
 int main(){
-    int arr[47] = {0};
-    int num;
-    cin>>num;
-    for(int i=0; i<=num+1; i++){
-        if(i == 0){
-            arr[0] = 0;
-        }else if (i == 1){
-            arr[1] = 1;
-        }else{
-            arr[i] = arr[i - 1] + arr[i - 2];
-        }
-        
-    }
-    cout<<arr[num]<<":"<<arr[num+1];
+    string word;
+    getline(cin, word);
+    word.erase(remove_if(word.begin(), word.end(),::isspace), word.end());
+    cout<<word.length();
     return 0;
 }
