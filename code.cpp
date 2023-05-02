d3cb4fa1755bfc52f306d8dddb649fba84d92ed6
bochain <bochainwu@gmail.com>
2023-05-02 22:22:10 +0800

    f341 	AC (4ms, 348KB)

--- code.cpp
+++ code.cpp
@@ -1,32 +1,32 @@
 #include<iostream>
-#include<cmath>
+#include<stack>
+#include<string>
 using namespace std;
 int main(){
-    string word;
-    int lucky, maxn=0, minn=101, letter[26] = {0};
+    string word, mid;
+    int num = 0;
+    stack <char> front, back;
     cin>>word;
-    for(int i=0;i<word.length();i++){
-        letter[(int)word[i] - 97] ++; 
+    cin>>mid;
+    num = word.find(mid);
+    for(int i=num + mid.length(); i<word.length();i++){
+        back.push(word[i]);
     }
 
-    for(int i=0; i<26;i++){
-        if(letter[i] > maxn) maxn = letter[i];
-        if(letter[i] < minn && letter[i] > 0) minn = letter[i];
-    }
-    bool isPrime = false;
-    int prime[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
-    for(auto p:prime){
-        if(maxn - minn == p){
-            isPrime = true;
-        }
-    }
-    if(isPrime){
-        cout<<"Lucky Word"<<endl;
-        cout<<maxn - minn;
-    }else{
-        cout<<"No Answer"<<endl;
-        cout<<'0';
+    for(int i=0; i<num;i++){
+        front.push(word[i]);
     }
     
+    while(! back.empty()){
+        char &tmp = back.top();
+        cout<<tmp;
+        back.pop();
+    }
+    cout<<mid;
+    while(! front.empty()){
+        char &tmp = front.top();
+        cout<<tmp;
+        front.pop();
+    }
     return 0;
 }
