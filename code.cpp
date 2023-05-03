e7d934b69c42d00d3c1e3765df288e0e62aa1b93
bochain <bochainwu@gmail.com>
2023-05-03 20:39:30 +0800

    e835 	AC (3ms, 340KB)

--- code.cpp
+++ code.cpp
@@ -1,60 +1,39 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int high, start, n;
-    bool right = false; 
-    cin>>n;
-    int hell[n];
-    for(int i=0; i<n; i++){
-        cin>>hell[i];
+    int a,area,row,colume;
+    while(cin>>a){
+        if(a<2500){
+            area=1;
+            if(a%25==0){
+                row=a/25;
+                colume=25;
+            }else{
+                row=(a/25)+1;
+                colume=(a%25);
+            }
+        }else if(2500<a&&a<=7500){
+            area = 2;
+            row=2;
+            a=a-2500;
+            if(a%50==0){
+                row=a/50;
+                colume=50;
+            }else{
+                row=(a/50)+1;
+                colume=(a%50);
+            }
+        }else if(7500<a&&a<=10000){
+            a=a-7500;
+            area=3;
+            if(a%25==0){
+                row=a/25;
+                colume=25;
+            }else{
+                row=(a/25)+1;
+                colume=(a%25);
+            }
+        }
+        cout<<area<<" "<<row<<" "<<colume;
     }
-    cin>>start;
-    start--;
-    if(start>1 && start<n-1){
-        if(hell[start - 1]>hell[start]){
-            right = true;
-        }else if (hell[start + 1]>hell[start]){
-            right = false;
-        }else{
-            if(hell[start]-hell[start+1] > hell[start]-hell[start-1]){
-                right = true;
-            }else{
-                right = false;
-            }
-        }
-    }else if (start == 0){
-        right = true;
-    }else if (start == n-1){
-        right = false;
-    }
-    
-    //cout<<right;
-    if(right){
-        while (start<n){
-            if(start == n-1){
-                cout<<start+1;
-                break;
-            }else if(hell[start+1]>hell[start]){
-                cout<<start+1;
-                break;
-            }else{
-                start++;
-            }
-            
-        }
-    }else{
-        while (start > -1){
-            if(start == 0){
-                cout<<start+1;
-                break;
-            }else if(hell[start-1]>hell[start]){
-                cout<<start+1;
-                break;
-            }else{
-                start--;
-            }
-            
-        }
-    }   
-    return 0;
 }
