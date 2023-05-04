389b2cbcc47c69816a92c8e5d24ca5b68293167f
bochain <bochainwu@gmail.com>
2023-05-04 22:55:39 +0800

    d860 AC (2ms, 328KB)

--- code.cpp
+++ code.cpp
@@ -1,33 +1,24 @@
 #include<iostream>
 using namespace std;
+long long gcd(long long x, long long y){
+    while ((x %= y) && (y %= x));
+    return x + y;
+}
 int main(){
-    int a[20000];
-    int k,ans=0;
-    cin>>k;
-    a[0]=6;
-    a[1]=2;
-    a[2]=5;
-    a[3]=5;
-    a[4]=4;
-    a[5]=5;
-    a[6]=6;
-    a[7]=3;
-    a[8]=7;
-    a[9]=6;
-    for(int i=10;i<=19999;i++){
-        int y=i;
-        int sum=0;
-        while (y!=0){
-            sum+=a[y%10];
-            y=y/10;
+    long long x0, y0;
+    cin>>x0>>y0;
+    if(x0 == y0){
+        cout<<1;
+    }else{
+        long long n = x0*y0;
+        long long ans = 0;
+        for (long long p = x0; p*p < n; p++){
+            if (n % p == 0 && gcd(p, n/p) == x0){
+                //是最小公倍數 && 最大公因數 = x0
+                ans += 2;
+            }
         }
-        a[i]=sum;
-     }
-    for(int A=0;A<=10000;A++){
-        for(int B=0;B<=10000;B++){
-            if (a[A]+a[B]+a[A+B]+4==k) ans=ans+1;
-        }
+        cout << ans << "\n";
     }
-    cout<<ans;
     return 0;
 }
