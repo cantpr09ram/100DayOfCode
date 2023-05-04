5de0aab3faaa2f8243cdc5437b1811c406e04786
bochain <bochainwu@gmail.com>
2023-05-04 22:58:33 +0800

    e320 AC (2ms, 328KB)

--- code.cpp
+++ code.cpp
@@ -1,24 +1,9 @@
 #include<iostream>
 using namespace std;
-long long gcd(long long x, long long y){
-    while ((x %= y) && (y %= x));
-    return x + y;
-}
 int main(){
-    long long x0, y0;
-    cin>>x0>>y0;
-    if(x0 == y0){
-        cout<<1;
-    }else{
-        long long n = x0*y0;
-        long long ans = 0;
-        for (long long p = x0; p*p < n; p++){
-            if (n % p == 0 && gcd(p, n/p) == x0){
-                //是最小公倍數 && 最大公因數 = x0
-                ans += 2;
-            }
-        }
-        cout << ans << "\n";
-    }
+    long long a, b;
+    cin>>a>>b;
+    cout<<a*b-a-b;
     return 0;
 }
+//https://zerojudge.tw/ShowThread?postid=20084&reply=0
