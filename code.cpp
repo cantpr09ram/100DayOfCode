79086e0346140f62a6c6018477b73ed342f4cb1a
bochain <bochainwu@gmail.com>
2023-05-04 10:13:04 +0800

    d108 	AC (0.2s, 408KB

--- code.cpp
+++ code.cpp
@@ -1,28 +1,33 @@
-#include <iostream>
-#include <algorithm>
+#include<iostream>
 using namespace std;
 int main(){
-    int n, l, r, ans, w;
-    cin>>w;
-    cin>>n;
-    int a[n];
-    for(int i=0; i<n; i++){
-        cin>>a[i];
-    }
-    sort(a, a+n);
-    l = 0;
-    r = n-1;
-    while(l < r){
-        if(a[r] + a[l] <= w){
-            l++;
-            r--;
-        }else{
-            r--;
+    int a[20000];
+    int k,ans=0;
+    cin>>k;
+    a[0]=6;
+    a[1]=2;
+    a[2]=5;
+    a[3]=5;
+    a[4]=4;
+    a[5]=5;
+    a[6]=6;
+    a[7]=3;
+    a[8]=7;
+    a[9]=6;
+    for(int i=10;i<=19999;i++){
+        int y=i;
+        int sum=0;
+        while (y!=0){
+            sum+=a[y%10];
+            y=y/10;
         }
-        ans++;
-    }
-    if(l == r){
-        ans++;
+        a[i]=sum;
+     }
+    for(int A=0;A<=10000;A++){
+        for(int B=0;B<=10000;B++){
+            if (a[A]+a[B]+a[A+B]+4==k) ans=ans+1;
+        }
     }
     cout<<ans;
+    return 0;
 }
