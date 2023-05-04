f1de47c089a59c0d116f7d48a8461964fd9c6baa
bochain <bochainwu@gmail.com>
2023-05-04 09:21:34 +0800

    b159 	AC (19ms, 428KB)

--- code.cpp
+++ code.cpp
@@ -1,39 +1,28 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main(){
-    int a,area,row,colume;
-    while(cin>>a){
-        if(a<2500){
-            area=1;
-            if(a%25==0){
-                row=a/25;
-                colume=25;
-            }else{
-                row=(a/25)+1;
-                colume=(a%25);
-            }
-        }else if(2500<a&&a<=7500){
-            area = 2;
-            row=2;
-            a=a-2500;
-            if(a%50==0){
-                row=a/50;
-                colume=50;
-            }else{
-                row=(a/50)+1;
-                colume=(a%50);
-            }
-        }else if(7500<a&&a<=10000){
-            a=a-7500;
-            area=3;
-            if(a%25==0){
-                row=a/25;
-                colume=25;
-            }else{
-                row=(a/25)+1;
-                colume=(a%25);
-            }
-        }
-        cout<<area<<" "<<row<<" "<<colume;
+    int n, l, r, ans, w;
+    cin>>w;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
     }
+    sort(a, a+n);
+    l = 0;
+    r = n-1;
+    while(l < r){
+        if(a[r] + a[l] <= w){
+            l++;
+            r--;
+        }else{
+            r--;
+        }
+        ans++;
+    }
+    if(l == r){
+        ans++;
+    }
+    cout<<ans;
 }
