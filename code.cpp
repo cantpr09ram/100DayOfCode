03c0eca8051df945647091250c6bd228c31620aa
bochain <bochainwu@gmail.com>
2023-05-06 23:53:09 +0800

    keep change

--- code.cpp
+++ code.cpp
@@ -4,7 +4,7 @@ int main() {
     cout.sync_with_stdio(false);
     cin.tie(0);
     long long a, b;
-    while(cin>>a){
+    for(a =1; a<100000;a++){
         b = a/2 +1;
         //cout<<b<<" ";
         cout<<(b+1)*b/2<<"\n";
