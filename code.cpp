660d033b8bc2673c2e75982491916d378ae2390e
bochain <bochainwu@gmail.com>
2023-05-09 23:56:45 +0800

    f819 	AC (5ms, 372KB)

--- code.cpp
+++ code.cpp
@@ -1,46 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int p[3];
-    string n;
-    cin>>p[0]>>p[1]>>p[2];
+    int money = 0, book, day,n;
     cin>>n;
-    for(int i=0;i<n.size();i++){
-        if(p[i] != '-'){
-            cout<<n[i];
-        }else if (n[i-1] >= n[i+1]){
-            cout<<n[i];
-        }else if (n[i-1]+1 == n[i+1]){
-            continue;
-        }else{
-            if(p[2] == '1'){
-                for(int j=p[i-1]+1;j<p[i+1];i++){
-                    for(int k=0;k<p[1];k++){
-                        if(p[0] == 1){
-                            cout<<(char)j;
-                        }else if(p[0] == 2){
-                            cout<<(char)j-26;
-                        }else{
-                            cout<<'*';
-                        }
-                    }
-                }
-            }else{
-                for(int j=p[i+1]+1;j>p[i+1];i--){
-                    for(int k=0;k<p[1];k++){
-                        if(p[0] == 1){
-                            cout<<(char)j;
-                        }else if(p[0] == 2){
-                            cout<<(char)j-26;
-                        }else{
-                            cout<<'*';
-                        }
-                    }
-                }
-            }
+    vector <int>book_list;
+    for(int i=0;i<n;i++){
+        cin>>book>>day;
+        if(day>100){
+            book_list.push_back(book);
+            money += (day-100)*5; 
         }
     }
+    sort(book_list.begin(), book_list.end());
+
+    for (auto it = book_list.begin(); it != book_list.end(); ++it) {
+        cout << *it << " ";
+    }
+
+    cout<<endl<<money;
+
     return 0;
 }
