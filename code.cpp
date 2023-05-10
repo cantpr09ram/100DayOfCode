02cc914456d81c93fe237bd74d9cd1c71ec62bf4
bochain <bochainwu@gmail.com>
2023-05-10 22:31:09 +0800

    e798 	AC (2ms, 352KB)

--- code.cpp
+++ code.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 int main() {
     //-------
     ios::sync_with_stdio(0);
     cin.tie(0);
     //-------
-    int n, ans=0, tmp;
-    vector <int> mountain;
-    cin>>n;
-    for (int i=0;i<n;i++) {
-        cin>>tmp;
-        if(mountain.empty() || tmp!=mountain[mountain.size()-1]){
-            mountain.push_back(tmp);
+    int a;
+    cin >> a ;
+    int b[a][a],c[4][a/2][a/2],d[4];
+    for(int i=0;i<a;i++){
+        for(int j=0;j<a;j++){
+            cin >> b[i][j] ;
         }
     }
-    for (int i=1; i<mountain.size()-1;i++) {
-        if(mountain[i-1]<mountain[i] && mountain[i]>mountain[i+1]){
-            ans++;
+
+    for(int i=1;i<a;i+=2){
+        for(int j=1;j<a;j+=2){
+            cout << max(b[i][j],max(b[i-1][j],max(b[i-1][j-1],b[i][j-1]))) << ' ' ;
         }
+        cout << endl ;
     }
-    cout<<ans;
+
     return 0;
 }
