62a7046fdae58ad2860cdd2b512ed7f68f89cf26
bochain <bochainwu@gmail.com>
2023-05-12 00:15:00 +0800

    #b163 	AC (2ms, 356KB)

--- code.cpp
+++ code.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
+#include <vector> 
+#include <cctype> //for islower(), isdgit(), tolower(), toupper()
+#include <algorithm> //for reverse()
 using namespace std;
-
-int main(void) {
-    int r, c;
-    //-------
+ 
+int main(void){
+    int p1, p2, p3;
+    string s0;
+    int left = 0, right;
+    vector<string> v; 
+    //========================== 
     ios::sync_with_stdio(0);
     cin.tie(0);
-    //-------
-    cin>>r>>c;
-    int a[r][c] = {0}, b[r][c] = {0};
-    for(int i=0;i<r;i++){
-        for(int j=0; j<c;j++){
-            cin>>a[i][j];
+    //========================== 
+    cin >> p1 >> p2 >> p3;
+    cin >> s0; 
+    while((right = s0.find('-', left)) != -1){
+        v.push_back(s0.substr(left, right-left));
+        if(!((islower(s0[right-1]) && islower(s0[right+1])||isdigit(s0[right-1]) && isdigit(s0[right+1]))&& (s0[right-1] < s0[right+1]))){ //check rule(1)
+            v.push_back("-"); 
+        }else{
+            string s1(""); 
+            for(char c=s0[right-1]+1; c<s0[right+1]; c++){
+                for(int i=0; i<p2; i++){
+                    if(p1 == 1) s1 += tolower(c);
+                    else if(p1 == 2) s1 += toupper(c);
+                    else s1 += '*'; //p1 is 3
+                }
+            } 
+            if(p3 == 2) reverse(s1.begin(), s1.end()); 
+            v.push_back(s1);
         }
+        left = right+1;
     }
-    for(int i=0;i<r;i++){
-        for(int j=0; j<c;j++){
-            cin>>b[i][j];
-        }
-    }
-    
-    for(int i=0;i<r;i++){
-        for(int j=0; j<c;j++){
-            int sum = 0;
-            for(int k=0;k<c;k++) sum += b[i][k];
-            for(int k=0;k<r;k++) sum += b[k][j];
-            sum -= b[i][j];
-            cout<<(sum+a[i][j])%2<<" ";
-        }
-        cout<<endl;
-    }
+    v.push_back(s0.substr(left)); //最右邊的字串
+    for(auto s:v) cout << s;
     return 0;
 }
