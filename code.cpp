38c73826a5ab05b12e34f60db9022c9c4bbd3a7a
bochain <bochainwu@gmail.com>
2023-05-12 22:14:38 +0800

    g004 AC (2ms, 344KB)

--- code.cpp
+++ code.cpp
@@ -1,39 +1,46 @@
-#include <iostream>
-#include <vector> 
-#include <cctype> //for islower(), isdgit(), tolower(), toupper()
-#include <algorithm> //for reverse()
+#include <iostream> 
 using namespace std;
- 
+
 int main(void){
-    int p1, p2, p3;
-    string s0;
-    int left = 0, right;
-    vector<string> v; 
+//上下左右 x, y座標位移量
+    int dx[] = {-1, 1, 0, 0},dy[] = {0, 0, -1, 1};
+
+    int r, c;
+    int p, total, count;
+    
     //========================== 
     ios::sync_with_stdio(0);
     cin.tie(0);
     //========================== 
-    cin >> p1 >> p2 >> p3;
-    cin >> s0; 
-    while((right = s0.find('-', left)) != -1){
-        v.push_back(s0.substr(left, right-left));
-        if(!((islower(s0[right-1]) && islower(s0[right+1])||isdigit(s0[right-1]) && isdigit(s0[right+1]))&& (s0[right-1] < s0[right+1]))){ //check rule(1)
-            v.push_back("-"); 
-        }else{
-            string s1(""); 
-            for(char c=s0[right-1]+1; c<s0[right+1]; c++){
-                for(int i=0; i<p2; i++){
-                    if(p1 == 1) s1 += tolower(c);
-                    else if(p1 == 2) s1 += toupper(c);
-                    else s1 += '*'; //p1 is 3
-                }
-            } 
-            if(p3 == 2) reverse(s1.begin(), s1.end()); 
-            v.push_back(s1);
+    cin >> r >> c;
+    int a[r+2][c+2] = {}, b[r+2][c+2]; //加邊框 0 
+
+    for(int i=1; i<=r; i++){
+        for(int j=1; j<=c; j++){
+            cin >> a[i][j];
         }
-        left = right+1;
+    } 
+    for(int i=1; i<=r; i++){
+        for(int j=1; j<=c; j++){
+            if(a[i][j]){ //非零部變動
+                b[i][j] = a[i][j];
+             continue;
+            } 
+        total = count = 0;
+        for(int k=0; k<4; k++){
+            p = a[i+dx[k]][j+dy[k]];
+            if(p == 0) continue; //相鄰為 0 不考慮
+            total += p;
+            count++;
+        }
+        b[i][j] = total>0? total/count: 0;
+        }
     }
-    v.push_back(s0.substr(left)); //最右邊的字串
-    for(auto s:v) cout << s;
+    for(int i=1; i<=r; i++){
+        for(int j=1; j<=c; j++){
+            cout << b[i][j] << ' ';
+        }
+        cout << '\n';
+    } 
     return 0;
 }
