7a04f4092d39739ebb12552635915127db02eca4
bochain <bochainwu@gmail.com>
2023-05-12 22:46:12 +0800

    e970 	AC (3ms, 372KB)

--- code.cpp
+++ code.cpp
@@ -2,31 +2,23 @@
 using namespace std;
 
 int main(void){
-    int n, e, q;
+    int n;
     //========================== 
     ios::sync_with_stdio(0);
     cin.tie(0);
     //========================== 
     cin>>n;
     int a[n];
+    for(int i=0; i<n;i++) cin>>a[i];
+
+    int b = a[n-1], sum=0;
+
     for(int i=0;i<n;i++){
-        cin>>a[i];
-    }
-    cin>>e;
-    cin>>q;
-    for(int i=0;i<q;i++){
-        int b;
-        cin>>b;
-        for(int j=0; j<n;j++){
-            if(a[j] == b){
-                swap(e, a[j]);
-                e = b;
-                continue;
-            }
+        if((i+1)%b == 1){
+            sum += a[i];
         }
     }
-    for(int i=0;i<n;i++){
-        cout<<a[i]<<" ";
-    }
+    if(sum%n == 0) cout<<n<<" "<<a[n-1];
+    else cout<<sum%n<<" "<<a[sum%n - 1];
     return 0;
 }
