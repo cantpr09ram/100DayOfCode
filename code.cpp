42dacd96ddaba2a65fcf7666bf6de61e136e2325
bochain <bochainwu@gmail.com>
2023-05-15 08:14:19 +0800

    b606 AC (4ms, 404KB

--- code.cpp
+++ code.cpp
@@ -4,27 +4,31 @@
 using namespace std;
 
 int main(void){
-    priority_queue<int, vector<int>, greater<int>> arr;
+    
     long long n, ans=0, tmp;
     //========================== 
     ios::sync_with_stdio(0);
     cin.tie(0);
     //========================== 
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>tmp;
-        arr.push(tmp);
-    }
-    while(arr.size() > 1){
-        long long a = arr.top();
-        arr.pop();
-        long long b = arr.top();
-        arr.pop();
-        arr.push(a+b);
-        ans += a+b;
-        //cout<<a<<b<<a+b;
+    
+    while (cin >> n && n != 0){
+        priority_queue<int, vector<int>, greater<int>> arr;
+        ans = 0;
+        for(int i=0;i<n;i++){
+            cin>>tmp;
+            arr.push(tmp);
+        }
+        while(arr.size() > 1){
+            long long a = arr.top();
+            arr.pop();
+            long long b = arr.top();
+            arr.pop();
+            arr.push(a+b);
+            ans += a+b;
+            //cout<<a<<b<<a+b;
 
+        }
+        cout<<ans<<endl;
     }
-    cout<<ans;
     return 0;
 }
