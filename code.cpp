f34c955086d0c44af7f3fcca372e103e7f5b88ec
bochain <bochainwu@gmail.com>
2023-05-17 23:28:01 +0800

    d859 	AC (0.1s, 344KB)

--- code.cpp
+++ code.cpp
@@ -1,27 +1,20 @@
 #include <iostream> 
-#include <queue>
-#include <algorithm>
 using namespace std;
-double cmp(pair<double,string>a, pair<double, string>b){
-    return a.first > b.first;
+int ans = 1;
+void dfs(int n){
+    for(int i=1; i<=n/2; i++){
+        ans++;
+        dfs(i);
+    }
 }
 int main(void){
-    int n, tmp, p, l, w, r;;
+    int n;
     //========================== 
     ios::sync_with_stdio(0);
     cin.tie(0);
     //==========================
     cin>>n;
-    pair<double, string> arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i].second>>p>>l>>w>>r;
-        arr[i].first = (double)p*w*r/l;
-        
-    }
-
-    sort(arr,arr+n);
-    for(int i=n-1;i>=0;i--){
-        cout<<arr[i].second<<endl;
-    }
+    dfs(n);
+    cout<<ans;
     return 0;
 }
