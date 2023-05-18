bb892cb68661f830a32f0a720a72e1c9629faac4
bochain <bochainwu@gmail.com>
2023-05-18 10:43:46 +0800

    e799 AC (3ms, 348KB)

--- code.cpp
+++ code.cpp
@@ -1,20 +1,32 @@
 #include <iostream> 
 using namespace std;
-int ans = 1;
-void dfs(int n){
-    for(int i=1; i<=n/2; i++){
-        ans++;
-        dfs(i);
-    }
-}
+
 int main(void){
-    int n;
+    int n, m;
+    char ch;
     //========================== 
     ios::sync_with_stdio(0);
     cin.tie(0);
     //==========================
-    cin>>n;
-    dfs(n);
-    cout<<ans;
-    return 0;
+    cin>>n>>m;
+    cin>>ch;
+    char arr[n][m];
+    long long tmp;
+    for(int i=0;i<n;i++){
+        cin>>tmp;
+        for(int j=m-1;j>=0;j--){
+            if(tmp %2 == 1){
+                arr[i][j] = ch;
+            }else{
+                arr[i][j] = '.';
+            }
+            tmp /= 2;
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 }
