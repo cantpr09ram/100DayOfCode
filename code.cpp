0f07fbe5162533a09e12b2401c697358eb97a7ba
bochain <bochainwu@gmail.com>
2023-05-20 11:29:27 +0800

    a013 	AC (2ms, 324KB)

--- code.cpp
+++ code.cpp
@@ -1,38 +1,58 @@
-#include <iostream> 
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <cmath>
+
 using namespace std;
 
-bool recur_split(int x, int y, int z, int m);
- 
-int n, t[16]; //大小依題意
- 
-int main(void){
-    int total = 0;
-    
-    //========================== 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    //==========================
-    
-    cin >> n;
-    for(int i=0; i<n; i++ ){
-        cin >> t[i];
-        total += t[i];
+// 將羅馬數字轉換為阿拉伯數字
+int romanToDecimal(string romanNum) {
+    map<char, int> romanMap = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+    int decimalNum = 0;
+    for (int i = 0; i < romanNum.length(); i++) {
+        int currentNum = romanMap[romanNum[i]];
+        int nextNum = i < romanNum.length() - 1 ? romanMap[romanNum[i + 1]] : 0;
+        decimalNum += currentNum < nextNum ? -currentNum : currentNum;
     }
-    
-    if(total % 3) cout << "NO";
-    else cout << (recur_split(total/3, total/3, total/3, 0)?"YES":"NO");
-    
-    return 0;
+    return decimalNum;
 }
- 
-bool recur_split(int x, int y, int z, int m){
-    if(!x && !y && !z) return true; //完成題目要求的分配
-    
-    if(x >= t[m] && recur_split(x-t[m], y, z, m+1)) return true;
-    
-    if(y >= t[m] && recur_split(x, y-t[m], z, m+1)) return true;
-    
-    if(z >= t[m] && recur_split(x, y, z-t[m], m+1)) return true;
-    
-    return false;
+
+// 將阿拉伯數字轉換為羅馬數字
+string intToRoman(int num) {
+    string romanNum = "";
+    int nums[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    string symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    for (int i = 0; i < 13; i++) {
+        while (num >= nums[i]) {
+            romanNum += symbols[i];
+            num -= nums[i];
+        }
+    }
+    return romanNum;
+}
+
+// 計算兩個羅馬數字的差的絕對值，並返回羅馬數字
+string romanAbsDiff(string romanNum1, string romanNum2) {
+    int diff = romanToDecimal(romanNum1) - romanToDecimal(romanNum2);
+    if (diff == 0) {
+        return "ZERO";
+    }
+    int absDiff = abs(diff);
+    return intToRoman(absDiff);
+}
+
+// 主函數
+int main() {
+    // 計算兩個羅馬數字的差的絕對值，並輸出結果
+    string romanNum1 ;
+    string romanNum2 ;
+    while(cin>>romanNum1){
+    if(romanNum1[0]=='#')
+    break;
+    cin>>romanNum2;
+    cout << romanAbsDiff(romanNum1, romanNum2) << endl;
+    }
+
+    return 0;
 }
