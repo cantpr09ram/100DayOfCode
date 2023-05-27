3080eeaae4a17a30861d0564ff0bbed8f45855d7
bochain <bochainwu@gmail.com>
2023-05-27 21:49:51 +0800

    a647 	AC (5ms, 332KB)

--- code.cpp
+++ code.cpp
@@ -4,42 +4,29 @@
 using namespace std;
 
 int main() {
+	
     //========================== 
     ios::sync_with_stdio(0);
     cin.tie(0);
     //==========================
-	vector <int> arr;
-	int tmp, n;
-	while (cin>>tmp && tmp != 0){
-		arr.push_back(tmp);
-	}
-	cin>>n;
+    int n, m, p;
+    cin>>n;
+    for(int i=0; i<n;i++){
+        cin>>m>>p;
+        float x = ((float)p/(float)m - 1)*100;
+        if(x >= 0){
+            x += +0.00001;
+        }else{
+            x -= +0.00001;
+        }
+        char c='%';
+        printf("%.2f%c ", x, c);
 
-	for(int j=0;j<n;j++){
-		int arr1[arr.size()] = {0};
-		if(arr[0]>arr[1]){
-			arr1[1] += arr[0]*0.1;
-		}
-
-		for(int i=1;i<arr.size()-1;i++){
-			if(arr[i]>arr[i-1]){
-				arr1[i-1] += arr[i]*0.05;
-			}
-			if(arr[i]>arr[i+1]){
-				arr1[i+1] += arr[i]*0.05;
-			}
-		}
-		
-		if(arr[arr.size()-1]>arr[arr.size()-2]){
-			arr1[arr.size()-2] += arr[arr.size()-1]*0.1;
-		}
-		
-		for(int i=0;i<arr.size();i++){
-			arr[i]+=arr1[i];
-		}
-	}
-	for(int i=0;i<arr.size();i++){
-		cout<<arr[i]<<" ";
-	}
+        if(x>=10.00 || x<=-7.00)
+            printf("dispose\n");
+        else
+            printf("keep\n");
+    }
     return 0;
 }
+    
