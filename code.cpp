e2ae533ede83ce804a8423173a352c57d5bbe2fa
bochain <bochainwu@gmail.com>
2023-05-28 10:38:49 +0800

    a738 	AC (2ms, 340KB)

--- code.cpp
+++ code.cpp
@@ -3,29 +3,18 @@
 #include <algorithm>
 using namespace std;
 
+int gcd(int a, int b){
+    return b == 0? a:gcd(b, a%b);
+}
+
 int main() {
-	
+	int a, b;
     //========================== 
     ios::sync_with_stdio(0);
     cin.tie(0);
     //==========================
-    int n, m, p;
-    cin>>n;
-    for(int i=0; i<n;i++){
-        cin>>m>>p;
-        float x = ((float)p/(float)m - 1)*100;
-        if(x >= 0){
-            x += +0.00001;
-        }else{
-            x -= +0.00001;
-        }
-        char c='%';
-        printf("%.2f%c ", x, c);
-
-        if(x>=10.00 || x<=-7.00)
-            printf("dispose\n");
-        else
-            printf("keep\n");
+    while(cin>>a>>b){
+        cout<<gcd(a,b)<<endl;
     }
     return 0;
 }
