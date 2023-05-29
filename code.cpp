4e5792bcd1c9af25a6a4fcc9b5132f12b1e5ec2d
bochain <bochainwu@gmail.com>
2023-05-29 09:24:23 +0800

    a915 	AC (5ms, 340KB)

--- code.cpp
+++ code.cpp
@@ -3,19 +3,31 @@
 #include <algorithm>
 using namespace std;
 
-int gcd(int a, int b){
-    return b == 0? a:gcd(b, a%b);
+bool cmp(pair<int, int>a, pair<int, int>b) {
+    if(a.first == b.first){
+        return a.second < b.second;
+    }else{
+        return a.first < b.first;
+    }
 }
 
 int main() {
-	int a, b;
+	int n, tmpa, tmpb;
+    vector<pair<int, int>>arr;
     //========================== 
     ios::sync_with_stdio(0);
     cin.tie(0);
     //==========================
-    while(cin>>a>>b){
-        cout<<gcd(a,b)<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>tmpa>>tmpb;
+        arr.push_back(make_pair(tmpa, tmpb));
     }
+    sort(arr.begin(), arr.end(), cmp);
+    for(int i=0;i<n;i++){
+        cout<<arr[i].first<<" "<<arr[i].second<<endl;
+    }
+
     return 0;
 }
     
