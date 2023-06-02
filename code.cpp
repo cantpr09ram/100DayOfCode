e3fd32974948ad51ad8117d97e450e7309bb59c9
bochain <bochainwu@gmail.com>
2023-06-02 21:04:51 +0800

    b970 	AC (3ms, 348KB)

--- code.cpp
+++ code.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <bitset>
-#include <cmath>
 using namespace std;
 
 int main() {
@@ -8,22 +6,10 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     //==========================
-    int n, x, y, yee;
+    int n;
     cin>>n;
-   while (n--){
-        cin>>x>>y;
-        yee = 100 - (y+x);
-        if(0<yee && yee<=30){
-            cout<<"sad!"<<endl;
-        }else if (30<yee && yee<=60){
-            cout<<"hmm~~"<<endl;
-        }else if (60<yee && yee<100){
-            cout<<"Happyyummy"<<endl;
-        }else{
-            cout<<"evil!!"<<endl;
-        }
-   }
-   
+    for(int i=0;i<n;i++){
+        cout<<i+1<<". I don't say swear words!"<<endl;
+    }
     return 0;
 }
-//d470
