bdd184124ade08557889b6d0a2ee596294d3f0eb
bochain <bochainwu@gmail.com>
2023-06-03 20:10:53 +0800

    d086  AC (2ms, 352KB)

--- code.cpp
+++ code.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+bool CheckIfAlphabet(char Input){
+    return (((Input>=65)&&(Input<=90))||((Input>=97)&&(Input<=122)));
+}
 
 int main() {
     //========================== 
     ios::sync_with_stdio(0);
     cin.tie(0);
     //==========================
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cout<<i+1<<". I don't say swear words!"<<endl;
+    string Input;
+    while(cin>>Input){
+        if(Input==to_string(0)){
+            break;
+        }
+        int Sum = 0;
+        for(int i=0;i<Input.length();i++){
+            if(CheckIfAlphabet(Input[i])){
+                Sum += (Input[i]<=93)?(Input[i]-'@'):(Input[i]-'`');
+            }else{
+                cout<<"Fail"<<'\n';
+                goto Next;
+            }
+        }
+        cout<<Sum<<'\n';
+        Next:{continue;}
     }
     return 0;
 }
